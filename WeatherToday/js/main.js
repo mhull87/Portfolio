ad0ee5a7e678ad2546df60cf011a6b4d75874dd5
Melissa Hull <melissahull87@gmail.com>
2021-06-15 21:51:53 -0500

    weather today

---
+++ WeatherToday/js/main.js
@@ -0,0 +1,26 @@
+//hamberger menu
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+var d = new Date();
+
+//copyright year
+document.getElementById("year").textContent = d.getFullYear();
+
+//Current date on footer
+var dow = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var day = dow[d.getDay()];
+var number = d.getDate();
+var m = month[d.getMonth()];
+var year = d.getFullYear();
+
+document.getElementById("date").innerHTML = (day + ", " + number + " " + m + " " + year);
+
+//Storm Severity Range Slider
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+
---
+++ WeatherToday/js/towns.js
@@ -0,0 +1,95 @@
+let townid = " ";
+let name = document.querySelector('.active').textContent;
+
+if (name == "Preston") {
+    townid = "id=5604473";
+} else if (name == "Soda Springs") {
+    townid = "id=5607916";
+} else if (name == "Fish Haven") {
+    townid = "lat=42.0380399&lon=-111.4048681";
+}
+
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?' + townid + '&units=imperial&APPID=9e82453a440594b3cdfb2221e87e0b94';
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+
+        var current = jsObject.weather[0].main;
+        var ftemp = jsObject.main.temp;
+        var wspeed = jsObject.wind.speed;
+        var hum = jsObject.main.humidity;
+        var wchill = 35.74 + (0.6215 * ftemp) - 35.75 * Math.pow((wspeed), .16) + .4275 * ftemp * Math.pow((wspeed), .16);
+        document.getElementById('currently').textContent = current;
+        document.getElementById('currentTemp').textContent = Math.round(ftemp);
+        document.getElementById('humidity').textContent = hum;
+        document.getElementById('speed').textContent = Math.round(wspeed);
+
+        if (ftemp <= 50 && wspeed >= 3) {
+            document.getElementById('chill').innerHTML = Math.round(wchill) + "&#8457;";
+        } else {
+            document.getElementById('chill').innerHTML = 'N/A';
+        }
+    });
+
+const getURL = 'https://api.openweathermap.org/data/2.5/forecast?' + townid + '&units=imperial&APPID=9e82453a440594b3cdfb2221e87e0b94';
+
+fetch(getURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+
+        const fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+
+        const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+        let day = 0;
+        fivedayforecast.forEach(forecast => {
+            let d = new Date(forecast.dt_txt);
+            const imagesrc = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+            const desc = forecast.weather[0].description;
+
+            document.getElementById(`forecast${day+1}`).textContent = Math.floor(forecast.main.temp);
+            document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
+            document.getElementById(`img${day+1}`).setAttribute('src', imagesrc);
+            document.getElementById(`img${day+1}`).setAttribute('alt', desc);
+
+            day++;
+        });
+    });
+
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+    .then(function(response) {
+        return response.json();
+    })
+
+    .then(function(jsonObject) {
+
+        const towns = jsonObject['towns'];
+
+        towns.forEach(town => {
+
+            if (town.name == name) {
+
+                var events = document.createElement('section');
+                var header = document.createElement('h3')
+                var list = document.createElement('ul');
+                var eventitems = town.events;
+
+                eventitems.forEach(eventitem => {
+                    var li = document.createElement('li');
+                    li.textContent = eventitem;
+                    list.appendChild(li);
+                });
+
+                header.textContent = `Upcoming Events`;
+
+                events.appendChild(header);
+                events.appendChild(list);
+
+                document.querySelector('div.events').appendChild(events);
+            }
+        });
+
+    });
